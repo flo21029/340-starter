6b4489f7065aa3e8ffcaea89d8d7ef9cc67f4814
manuelfloresay <manuelflores.a.y@gmail.com>
2024-10-01 20:25:46 -0300

    Added ejs files

--- server.js
+++ server.js
@@ -5,10 +5,18 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
@@ -27,4 +35,9 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+// Inedx route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
 })
