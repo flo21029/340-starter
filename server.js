5f0c4cff611dbd4941a089f83e421c33adb348fe
flo21029 <flo21029@byui.edu>
2024-10-05 21:47:04 -0300

    first error handling added

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
+const utilities = require("./utilities")
 
 /* ***********************
  * View Engine and Templates
@@ -25,6 +26,23 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -43,3 +61,4 @@ app.listen(port, () => {
 app.get("/", baseController.buildHome)
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
